106a52c5ac9d0043b7bb1e94586ec9d89eb0ce71
Kim, Jae Chan <kjc0210@gmail.com>
2014-09-20 16:39:07 +0900

    Implement get and set data via network
    Used zeromq for networking.
    Network protocols are arbitrary. It always can be changed.

--- dimint/client.py
+++ dimint/client.py
@@ -1,19 +1,39 @@
-class DiMintClient:
-    __hosts = []
-    __data = {}
-    __connected = False
+import json
+import zmq
 
-    def __init__(self, hosts):
-        self.__hosts = hosts
+
+class DiMintClient:
+    __host = None
+    __port = None
+
+    def __init__(self, host, port):
+        self.__host = host
+        self.__port = port
         self.__connect()
 
     def get(self, key, default=None):
+        if not self.__connected:
+            self.connect()
+            if not self.__connected:
+                return default
         key = self.__convert_key(key)
-        return self.__data.get(key, default)
+        self.__socket.send('GET::::{0}'.format(key).encode('utf-8'))
+        res = self.__socket.recv()
+        try:
+            return json.loads(res.decode('utf-8'))
+        except TypeError:
+            return default
 
     def set(self, key, value):
+        if not self.__connected:
+            self.connect()
+            if not self.__connected:
+                return False
         key = self.__convert_key(key)
-        self.__data[key] = value
+        self.__socket.send('SET::::{0}::::{1}'.format(
+            key, json.dumps(value)).encode('utf-8'))
+        res = self.__socket.recv()
+        return json.loads(res.decode('utf-8'))
 
     def __getitem__(self, key):
         return self.get(key)
@@ -22,15 +42,29 @@ class DiMintClient:
         self.set(key, value)
 
     def __connect(self):
-        self.__connected = True
-        print('connected')
+        self.__context = zmq.Context()
+        self.__socket = self.__context.socket(zmq.REQ)
+        try:
+            self.__socket.connect('tcp://{0}:{1}'.format(
+                self.__host, self.__port))
+            self.__connected = True
+        except:
+            pass
 
     def __disconnect(self):
-        self.__connected = False
-        print('disconnected')
+        if self.__connected:
+            self.__socket.disconnect('tcp://{0}:{1}'.format(
+                self.__host, self.__port))
+            self.__connected = False
 
     def __convert_key(self, key):
-        return str(key)
+        key = str(key)
+        if '::::' in key:
+            raise KeyError('"::::" cannot be contained in key')
+        return key
 
     def __del__(self):
-        self.__disconnect()
+        try:
+            self.__disconnect()
+        except ImportError:
+            pass
