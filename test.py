7d38279078c79d40a5b0955300949564e242de8b
yeomii <yeomii999@gmail.com>
2014-12-13 00:34:30 +0900

    modified test.py

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from dimint_client import DiMintClient
 
-client = DiMintClient('127.0.0.1', 5556)
+c1 = DiMintClient('127.0.0.1', 5556)
+c2 = DiMintClient('127.0.0.1', 5556)
 
-print(client.set('key1', 'asdf'))
-print(client['key1'])
+print(c1.set('key1', 'asdf'))
+print(c2.set('key2', 'fsdfsd'))
+print(c1['key1'])
+print(c2['key2'])
